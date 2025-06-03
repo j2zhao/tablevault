44ce50b83c922bc37acddb11f992a29872ecafea
Jinjin Zhao <j2zhao@uchicago.edu>
2025-06-03 18:40:09 -0500

    debug windows issue

--- tablevault/_helper/metadata_store.py
+++ tablevault/_helper/metadata_store.py
@@ -359,7 +359,9 @@ class MetadataStore:
         before_time: Optional[float] = None,
         active_only: bool = True,
     ) -> tuple[float, float, str]:
+        print("GET TABLE")
         table_history = self._get_table_history()
+        print(table_history)
         max_changed_time = 0
         max_start_time = 0
         max_id = ""
@@ -371,12 +373,15 @@ class MetadataStore:
                 and version != ""
                 and not instance_id.startswith(version)
             ):
+                print("HELLO1")
                 continue
             if active_only and end_time is not None:
+                print("HELLO2")
                 continue
             if start_time > max_start_time and (
                 before_time is None or start_time < before_time
             ):
+                print("HELLO3")
                 max_start_time = start_time
                 max_changed_time = changed_time
                 max_id = instance_id
